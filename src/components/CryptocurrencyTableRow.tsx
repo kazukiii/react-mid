57503a4bfe9f135aa670bfadaa982ca3e229b3f0
kazukiii <kazumatsu.dev28@gmail.com>
2023-04-25 16:55:16 -0700

    convert number to locale string

--- src/components/CryptocurrencyTableRow.tsx
+++ src/components/CryptocurrencyTableRow.tsx
@@ -18,8 +18,8 @@ const CryptocurrencyTableRow: FC<CryptocurrencyTableRowProps> = ({ row }) => {
         {row.name}
       </TableCell>
       <TableCell>{row.symbol}</TableCell>
-      <TableCell sx={{ whiteSpace: 'nowrap' }}>$ {row.market_cap}</TableCell>
-      <TableCell sx={{ whiteSpace: 'nowrap' }}>$ {row.current_price}</TableCell>
+      <TableCell sx={{ whiteSpace: 'nowrap' }}>$ {row.market_cap.toLocaleString()}</TableCell>
+      <TableCell sx={{ whiteSpace: 'nowrap' }}>$ {row.current_price.toLocaleString()}</TableCell>
       <TableCell>{format(new Date(row.last_updated), 'yyyy-MM-dd HH:mm:ss')}</TableCell>
     </TableRow>
   )
