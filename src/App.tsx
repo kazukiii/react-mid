84185fd13cf418239518b5f3e8eb220219e7dcab
kazukiii <kazumatsu.dev28@gmail.com>
2023-04-25 00:14:06 -0700

    update: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,98 @@
-import CryptocurrencyTable from "./components/CryptocurrencyTable"
-import { Box, Slider, TextField } from "@mui/material"
-import { SyntheticEvent, useRef, useState } from "react"
-import Header from "./components/Header"
-import { filter } from "lodash"
-import { useFetch } from "./hooks/useFetch"
-import Button from "@mui/material/Button"
-import axios from "axios"
-import { useMediaQuery } from "./hooks/useMediaQuery";
+import React, { SyntheticEvent, useState } from 'react'
+import axios from 'axios'
+import { filter } from 'lodash'
+import Button from '@mui/material/Button'
+import { Box, Slider, TextField } from '@mui/material'
+import CryptocurrencyTable from './components/CryptocurrencyTable'
+import Header from './components/Header'
+import { useFetch } from './hooks/useFetch'
+import { useMediaQuery } from './hooks/useMediaQuery'
+import { Cryptocurrency } from '../types/cryptocurrency'
 
-function App() {
-    const { data: rows, isLoading, isError, setData } = useFetch('/coins/markets', { vs_currency: 'usd', per_page: 300 })
-    const [value, setValue] = useState<number[]>([0, 30000])
-    const [nameForSearch, setNameForSearch] = useState<string>('')
-    const isSp = useMediaQuery('md')
+const App: React.FC = () => {
+  const { data: rows, isLoading, isError, setData } = useFetch('/coins/markets', { vs_currency: 'usd', per_page: 300 })
+  const minSliderValue = 0
+  const maxSliderValue = 30000
+  const [value, setValue] = useState<number[]>([minSliderValue, maxSliderValue])
+  const [nameForSearch, setNameForSearch] = useState<string>('')
+  const isSp = useMediaQuery('md')
 
-    const search = async (name: string, value: number[]) => {
-        const { data } = await axios.get('https://api.coingecko.com/api/v3/coins/markets', { params: { vs_currency: 'usd', per_page: 300 } })
+  const search = async (name: string, value: number[]) => {
+    const { data } = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+      params: { vs_currency: 'usd', per_page: 300 },
+    })
 
-        if (nameForSearch) {
-            setData(filter(
-                filter(data, (row) => row['name'].toLowerCase() === nameForSearch.toLowerCase()),  (row) => value[0] <= row['current_price'] && row['current_price'] <= value[1]
-            ))
-        } else {
-            setData(filter(data, (row) => value[0] <= row['current_price'] && row['current_price'] <= value[1]))
-        }
+    if (nameForSearch) {
+      setData(
+        filter(
+          filter(data, (row) => row['name'].toLowerCase() === nameForSearch.toLowerCase()),
+          (row) => value[0] <= row['current_price'] && row['current_price'] <= value[1]
+        )
+      )
+    } else {
+      setData(filter(data, (row) => value[0] <= row['current_price'] && row['current_price'] <= value[1]))
     }
-    const handleChange = async (event: SyntheticEvent | Event, newValue: number | number[]) => {
-        setValue(newValue as number[])
-        await search(nameForSearch, value)
-    }
+  }
+  const handleChange = async (event: SyntheticEvent | Event, newValue: number | number[]) => {
+    setValue(newValue as number[])
+    await search(nameForSearch, value)
+  }
 
-    const handleInputChange = (event: Object) => {
-        setNameForSearch((event as any).target.value)
-    }
+  const handleInputChange = (event: SyntheticEvent | Event) => {
+    setNameForSearch((event as any).target.value)
+  }
 
-    const handleClick = async (nameForSearch: string) => {
-        await search(nameForSearch, value)
-    }
+  const handleClick = async (nameForSearch: string) => {
+    await search(nameForSearch, value)
+  }
 
-    return (
-        <div>
-            <Header />
-            <h1 style={{ display: 'flex', justifyContent: 'center', marginTop: 50}}>
-                Cryptocurrencies
-            </h1>
-            <div style={isSp
-                ? { display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 200}
-                : {display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20}}
-            >
-                <div style={{display: "flex", alignItems: 'center'}}>
-                    <TextField
-                        id="name"
-                        label="Search by name here"
-                        variant="standard"
-                        style={{ width: 250, height: 30, marginBottom: 30 }}
-                        autoComplete="off"
-                        onChange={handleInputChange}
-                        onKeyDown={async (e) => {
-                            console.log(e.key, '1111')
-                            if (e.key === 'Enter') {
-                                e.preventDefault()
-                                handleInputChange(e)
-                                await handleClick(nameForSearch)
-                            }
-                        }}
-                    />
-                    <Button onClick={() => handleClick(nameForSearch)}>Search</Button>
-                </div>
-                <div>
-                    <p>Price range</p>
-                    <Box sx={{ width: 300 }}>
-                        <Slider
-                            getAriaLabel={() => 'Temperature range'}
-                            value={value}
-                            onChange={handleChange}
-                            onChangeCommitted={handleChange}
-                            valueLabelDisplay="auto"
-                            max={30000}
-                        />
-                    </Box>
-                </div>
-            </div>
-            <div style={{ margin: 50}}>
-                <CryptocurrencyTable
-                    rows={rows}
-                    isLoading={isLoading}
-                    isError={isError}
-                    setData={setData}
-                />
-            </div>
+  return (
+    <div>
+      <Header />
+      <h1 className="flex justify-center mt-12">Cryptocurrencies</h1>
+      <div className={isSp ? 'flex justify-center items-center gap-52' : 'flex flex-col items-center gap-5'}>
+        <div className="flex items-center">
+          <TextField
+            id="name"
+            label="Search by name here"
+            variant="standard"
+            className="w-60 h-full mb-8"
+            autoComplete="off"
+            onChange={handleInputChange}
+            onKeyDown={async (e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+                handleInputChange(e)
+                await handleClick(nameForSearch)
+              }
+            }}
+          />
+          <Button onClick={() => handleClick(nameForSearch)}>Search</Button>
         </div>
-    )
+        <div>
+          <p>Price range</p>
+          <Box sx={{ width: 300 }}>
+            <Slider
+              getAriaLabel={() => 'Temperature range'}
+              value={value}
+              onChange={handleChange}
+              onChangeCommitted={handleChange}
+              valueLabelDisplay="auto"
+              max={maxSliderValue}
+            />
+          </Box>
+        </div>
+      </div>
+      <div className="m-12">
+        <CryptocurrencyTable
+          rows={rows as Cryptocurrency[]}
+          isLoading={isLoading}
+          isError={isError}
+          setData={setData}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,26 +1,34 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react'
 
-export const sizes: any = {
-    sm: '640px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px',
-    '2xl': '1536px',
-};
+type ScreenSizes = {
+  [key: string]: string
+}
 
-export const useMediaQuery = (screen: string): boolean => {
-    const [matches, setMatches] = useState(false);
+export const sizes: ScreenSizes = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
+}
 
-    useEffect(() => {
-        const query = `(min-width: ${sizes[screen]})`;
-        const media = window.matchMedia(query);
-        if (media.matches !== matches) {
-            setMatches(media.matches);
-        }
-        const listener = () => setMatches(media.matches);
-        window.addEventListener('resize', listener);
-        return () => window.removeEventListener('resize', listener);
-    }, [matches, screen]);
+export const useMediaQuery = (screen: keyof ScreenSizes): boolean => {
+  const [matches, setMatches] = useState(false)
 
-    return matches;
-};
+  useEffect(() => {
+    const query = `(min-width: ${sizes[screen]})`
+    const media = window.matchMedia(query)
+
+    if (media.matches !== matches) {
+      setMatches(media.matches)
+    }
+
+    const listener = () => setMatches(media.matches)
+
+    window.addEventListener('resize', listener)
+
+    return () => window.removeEventListener('resize', listener)
+  }, [matches, screen])
+
+  return matches
+}
